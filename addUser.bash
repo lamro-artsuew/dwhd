1c8666fc51a6397d4730d707f79c43c0ea8db1c3
Alisher Alikulov <139641630+lamro-artsuew@users.noreply.github.com>
2025-07-14 11:20:40 +0500

    Create addUser.bash

---
+++ addUser.bash
@@ -0,0 +1,7 @@
+sudo useradd -m -d /srv/minio minio
+sudo useradd -m -d /srv/clickhouse clickhouse
+sudo useradd -m -d /srv/airflow airflow
+sudo useradd -m -d /srv/airbyte airbyte
+sudo useradd -m -d /srv/kafka kafka
+sudo useradd -m -d /srv/superset superset
+sudo useradd -m -d /srv/monitoring monitoring
